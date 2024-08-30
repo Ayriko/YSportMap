f51aba4ab06cad81d2b3b3125f17e47a774ff830
amoiska <aymeric.moiska@ynov.com>
2024-08-30 18:46:44 +0200

    fix: remove previous code from testing now blocking vercel deployment

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,31 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import {Equipment} from "../types/equipment.ts";
-import client from "../client/client.tsx";
+import React from 'react';
 
 function DetailPage() : React.JSX.Element {
-    const [data, setData] = useState<Equipment[] | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const result = await client.getEquipments();
-                setData(result);
-            } catch (err) {
-                setError('Failed to fetch data: ' + err);
-            } finally {
-                setLoading(false);
-            }
-        })();
-    }, []);
-
     return (
         <div>
-            <h2>Fetched Data:</h2>
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         </div>
     );
 }
