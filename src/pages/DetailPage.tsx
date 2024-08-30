82e0b7cf5b38adafd2cc2e4e39cfcb975d5b5a60
amoiska <aymeric.moiska@ynov.com>
2024-08-31 00:33:32 +0200

    feat: detail page - need improvements during last day

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Equipment} from "../types/equipment.ts";
+import { useLocation } from "react-router-dom";
+import client from "../client/client.tsx";
+import Header from "../component/Header.tsx";
+import Footer from "../component/Footer.tsx";
+import EquipmentDetail from "../component/EquipmentDetail.tsx";
 
 function DetailPage() : React.JSX.Element {
+    const location = useLocation();
+    const num_equip = location.state as string;
+    const [data, setData] = useState<Equipment>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await client.getEquipmentByNumero(num_equip);
+            if (result) {
+                setData(result);
+            } else {
+                console.error('No data returned from the API.');
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
-        <div>
+        <div className="min-h-screen">
+            <Header />
+            <EquipmentDetail data={data} />
+            <Footer />
         </div>
     );
 }
